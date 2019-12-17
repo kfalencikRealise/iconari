0eb01485c223ae9c6569d0278a0bef0eeba91cd3
kfalencik <kevin.falencik@amazerealise.com>
2019-12-17 16:50:24 +0000

    Product page

--- pages/shop/_slug.vue
+++ pages/shop/_slug.vue
@@ -1,6 +1,28 @@
 <template>
-  <div class="container">
-    {{ product.title }}
+  <div class="product container">
+    <div class="columns">
+      <div class="product__image column is-half">
+        <img :src="require('@/assets/products/' + product.image)" />
+      </div>
+
+      <div class="product__details column is-half">
+        <div class="product__title">
+          <h3>{{product.title}}</h3>
+        </div>
+        <div class="product__price">
+          <template v-if="product.discount">
+            <span class="discount">${{ product.price }}</span>
+            <span class="price"><strong>${{ discount(product.price, product.discount) }}</strong></span>
+          </template>
+          <template v-else>
+            <span class="price"><strong>${{ product.price }}</strong></span>
+          </template>
+        </div>
+        <div class="product__description">
+          {{product.description}}
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -20,5 +42,16 @@ export default {
       return product[0];
     }
   },
+  methods: {
+    discount: function(price, discount) {
+      return parseInt(price - ((price / 100) * discount));
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+  .product {
+
+  }
+</style>
