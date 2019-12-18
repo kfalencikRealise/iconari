65f4d9c099bc98f4d8392ece4038980435fc6e98
kfalencik <kevin.falencik@amazerealise.com>
2019-12-18 16:24:16 +0000

    Fix zoom levels for portrait canvases

--- pages/shop/_slug.vue
+++ pages/shop/_slug.vue
@@ -2,7 +2,7 @@
   <div :class="{'product container': true, 'product--landscape': product.landscape}">
     <div class="columns is-4">
       <div class="column is-half">
-        <div class="product__image" @mouseover="magnify(2.5)" @mouseleave="magnify(1)">
+        <div class="product__image" @mouseover="magnify(zoomLevel)" @mouseleave="magnify(1)">
           <div class="product__canvas" :style="{ 'background-image': 'url(' + require('@/assets/products/' + product.canvasImage) + ')', 'transform': 'scale(' + size * zoom + ')' }">
             <div class="product__canvas-top" :style="{'height': thickness + 'px', 'top': thickness * -1 + 'px', 'margin-left': thickness/2 + 'px', 'background': edge}"></div>
             <div class="product__canvas-right" :style="{'width': thickness + 'px', 'margin-top': thickness * -1 + 'px', 'margin-left': thickness + 'px', 'background': edge}"></div>
@@ -145,6 +145,9 @@ export default {
   computed: {
     slug() {
       return this.$route.params.slug;
+    },
+    zoomLevel() {
+      return this.product.landscape ? 2.5 : 2;
     },
     product() {
       const product = this.$store.state.products.filter(product => product.slug === this.slug);
